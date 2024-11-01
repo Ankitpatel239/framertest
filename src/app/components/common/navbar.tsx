2a3371158f8412fb04ba0c9e71054f62ede72154
Ankitpatel239 <141391740+Ankitpatel239@users.noreply.github.com>
2024-11-01 21:54:27 +0530

    Initial commit from Create Next App

---
+++ src/app/components/common/navbar.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div>
+      hii navbar
+    </div>
+  )
+}
+
+export default Navbar;
---
+++ src/app/layout.tsx
@@ -0,0 +1,35 @@
+import type { Metadata } from "next";
+import localFont from "next/font/local";
+import "./globals.css";
+
+const geistSans = localFont({
+  src: "./fonts/GeistVF.woff",
+  variable: "--font-geist-sans",
+  weight: "100 900",
+});
+const geistMono = localFont({
+  src: "./fonts/GeistMonoVF.woff",
+  variable: "--font-geist-mono",
+  weight: "100 900",
+});
+
+export const metadata: Metadata = {
+  title: "Ankit Patel | Web-Developer",
+  description: "I am Ankit, a passionate web developer with a strong background in creating innovative and efficient solutions. Feel free to connect with me for any inquiries or collaborations!",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
